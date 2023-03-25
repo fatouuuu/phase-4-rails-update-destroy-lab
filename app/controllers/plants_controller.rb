d38674585b50346573512236ef7cd99da3ba72b4
fatouuuu <fatmaabdullrahim@gmail.com>
2023-03-25 14:52:40 +0300

    optional lab

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant.update(plant_params)
+      render json: plant
+    else
+      render json: { errors: plant.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+  
+
   private
 
   def plant_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  # resources :plants, only: [:index, :show, :create. :update, :destroy]
+  resources :plants
+  # patch '/plants/:id', to: 'plants#update'
+  # delete '/plants/:id', to: 'plants#destroy'
 end
